e85f73a0f5380913be2e8ebf58c0253ff903afd5
mnawaz <mnawaz@thoughtworks.com>
2018-05-23 23:46:22 +0530

    Add new code flow to interact with Database

--- app/AirfareMain.go
+++ app/AirfareMain.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/nawazish-github/airfare-poller/database"
 	"github.com/nawazish-github/airfare-poller/models"
 
 	"github.com/nawazish-github/airfare-poller/config"
@@ -19,6 +21,7 @@ func main() {
 		fmt.Println(configError)
 	}
 
+	log.Output(0, "Successfully read the Config file!")
 	url := getURL(config)
 	var airfareHttpClient http.AirfareHttpClient
 	data, urlError := airfareHttpClient.Get(url)
@@ -27,6 +30,7 @@ func main() {
 		fmt.Println(urlError)
 		return
 	}
+	log.Output(0, "Successfully queried the remote service!")
 	var jsonUnmarshaller unmarshal.JsonUnmarshaller
 	airfareResp, airfareRespError := jsonUnmarshaller.Unmarshal(data)
 
@@ -34,8 +38,39 @@ func main() {
 		fmt.Println(airfareRespError)
 		return
 	}
+	//fmt.Println(airfareResp)
+	log.Output(0, "Successfully unmarshalled the remote response!")
 
-	fmt.Println(airfareResp)
+	/* get data from DB to compare;
+	   if DB has no data, simply insert all data into it;
+	   if DB has data in it, update (upsert) delta into it*/
+
+	var databaseClient database.DatabaseClient
+	_, dbConErr := databaseClient.DialDBAt("localhost", "airfare-poller-1", "airfare_poller_collection_one")
+	if dbConErr != nil {
+		panic(dbConErr)
+	}
+
+	log.Output(0, "Successfully dialed into database server!")
+	response, dbDataErr := databaseClient.GetDataFor("BLR-IXR")
+	if dbDataErr != nil {
+		panic(dbDataErr)
+	}
+
+	log.Output(100, "Successfully queried Database for archived data!")
+	if len(response) == 0 {
+		dbUpdateError := databaseClient.UpdateDataFor("BLR-IXR", airfareResp)
+		if dbUpdateError != nil {
+			panic(dbUpdateError)
+		}
+		log.Output(0, "database updated successfully with new data...enjoy")
+	}
+
+	log.Output(0, "verifying delta in two data sets...")
+
+	/*compare deltas between http airfareresponse and db airfareresponse
+	  and*/
+
 }
 
 func getURL(config *models.Config) string {
